1e82be7cb65594c7b5454f2d985196392ce26959
Ivan Fomin <la-verne@mail.ru>
2019-02-21 10:45:11 +0300

    Delete vendor

--- vendor/tamtamchik/simple-flash/examples/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once 'bootstrap3.php';
--- vendor/tamtamchik/simple-flash/src/Exceptions/FlashTemplateException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tamtamchik\SimpleFlash\Exceptions;
-
-/**
- * Class FlashTemplateException.
- * Thrown when there is a problem with template class.
- */
-class FlashTemplateException extends \Exception { }
